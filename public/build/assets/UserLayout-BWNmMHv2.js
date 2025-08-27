077fe54b0b4a804263740898873053cbeafc118f
TranThanhQuan <quanlive123@gmail.com>
2025-08-27 14:02:55 +0700

    merge from dev

---
+++ public/build/assets/UserLayout-BWNmMHv2.js
@@ -0,0 +1,1 @@
+import{i as r}from"./index-CMt5OxLb.js";import t from"./Header-BEgB4U0a.js";import a from"./Footer-D2fzYZ4a.js";import{h as s,b as m,o as n,a as o,r as i}from"./app-DzlCFq8e.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const b={__name:"UserLayout",setup(l){return s(()=>{r()}),(e,p)=>(n(),m("div",null,[o(t),i(e.$slots,"default"),o(a)]))}};export{b as default};
