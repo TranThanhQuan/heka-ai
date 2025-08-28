718912635f85dbc179308a2b4cb3a2a299bdc913
TranThanhQuan <quanlive123@gmail.com>
2025-08-28 16:48:37 +0700

    edit error elready vip

---
+++ public/build/assets/UserLayout-DuFIqlF7.js
@@ -0,0 +1,1 @@
+import{i as r}from"./index-CpCCuq2T.js";import t from"./Header-BSS4ki2r.js";import a from"./Footer-CS-pWx3u.js";import{h as s,b as m,o as n,a as o,r as i}from"./app-CvYeMjMh.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const b={__name:"UserLayout",setup(l){return s(()=>{r()}),(e,p)=>(n(),m("div",null,[o(t),i(e.$slots,"default"),o(a)]))}};export{b as default};
